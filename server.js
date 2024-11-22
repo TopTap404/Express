72602f485c4848e46694997a45ac0da47c671024
BuildTools <unconfigured@null.spigotmc.org>
2024-11-23 00:24:08 +0700

    update login and register api

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt');
 
 const app = express();
+const prisma = new PrismaClient();
 const port = 3001;  // Express port
 
 let temperature = null; // สร้างตัวแปรข้อมูลอุณหภูมิ
@@ -12,7 +15,7 @@ let humidity = null;
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://104.214.176.253/'
+  origin: 'http://104.214.176.253'
 }));
 
 // API POST สำหรับ ESP32 ให้ส่งข้อมูลเข้ามา
@@ -40,7 +43,6 @@ app.post("/updateHumidity", (req, res) => {
   }
 });
 
-
 // API สำหรับ React web ให้ดึงข้อมูลจาก Express ไปใช้
 app.get("/getTemperature", (req, res) => {
   if (temperature !== null) {
@@ -63,3 +65,54 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+app.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    const existingUser = await prisma.user_accout.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already exists.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user_accout.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: 'User registered successfully.', user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user_accout.findUnique({
+    where: { email: email }
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+
+  res.json({ message: 'Login successful' });
+});
